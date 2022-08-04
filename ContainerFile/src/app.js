e0d4e93b825d3a5b22abafc8cac30ef86fc50302
Tal Hason <thason@redhat.com>
2022-08-04 13:42:03 +0300

    updated node application

--- ContainerFile/src/app.js
+++ ContainerFile/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! From ArgoCD with working pipeline!, Hi Shiran :) \n');
+  res.send('Hello World! From ArgoCD with working Tekton pipeline!, ver.9 \n');
 });
 
 app.listen(8080, function () {
