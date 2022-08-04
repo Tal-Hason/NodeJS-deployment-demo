c096bb8fb5f71a901ad47df394ae369f3ba15724
Tal Hason <thason@redhat.com>
2022-08-04 11:12:43 +0300

    Hi shiran

--- ContainerFile/src/app.js
+++ ContainerFile/src/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! From ArgoCD with working pipeline! :) \n');
+  res.send('Hello World! From ArgoCD with working pipeline!, Hi Shiran :) \n');
 });
 
 app.listen(8080, function () {
